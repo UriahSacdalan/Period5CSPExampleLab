6fa2cc1e15812f94e2df5cc2032363a85e4b2d65
Uriah <uriahnaomi.sacdalan@vtsd.com>
2022-09-12 11:44:49 -0400

    Data Type Update

--- DataTypes.java
+++ DataTypes.java
@@ -14,8 +14,20 @@
 public class DataTypes {
 
     // Variable syntax: dataType varName = value;
-    int uriahFavInt = 5;
-    double snicker = 7.5;
-    boolean imaginePlayingSoccer = true;
+    public static void main(String args[]){
+        int uriahFavInt = 5;
+        double snicker = 7.5;
+        boolean imaginePlayingSoccer = true;
+
+        System.out.println("Uriah's Favourite Integer is:" + uriahFavInt + "and the class snicker was" + snicker);
+
+
+        int n1 = 4;
+        int n2 = 12;
+        System.out.print(n1 + n2 / n1);
+
+
+    }
+
 
 }
